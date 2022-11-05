b35595a0de376e4f83d439d9c6991de7717231a8
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-11-05 23:56:57 +0300

    Tasks 1 , 2 , 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for ( int i = 0; i <= 10; i++) {
+            System.out.println("Итерация: " + i);
+        }
+        int start = 10;
+        while (start > 0) {
+            System.out.println(start);
+            start --;
+        }
+        int startingNumber = 0;
+        boolean even = false;
+        for (startingNumber = 0; startingNumber <= 17; startingNumber++) {
+            if (startingNumber % 2 == 0) {
+                even = true;
+                System.out.println(startingNumber);
+        }
+        }
     }
 }
